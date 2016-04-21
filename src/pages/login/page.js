e1970f60d2790657ccdede10c59d810eded54319
Vladimir Sizov <sizov.v.f@gmail.com>
2016-04-21 21:06:20 +0100

    adding flipping card for test

--- src/pages/login/page.js
+++ src/pages/login/page.js
@@ -4,17 +4,47 @@ import styles from './style.css';
 
 
 export default class LoginPage extends React.Component {
-  signUp() {
-    browserHistory.push('/home');
-  }
-  
-  render() {
-    return (
-      <div className={styles.content}>
-        <h1 className={styles.heading}>Login Page</h1>
-        <p className={styles.lead}>Create an account to get started!</p>
-        <button className={styles.signUpButton} onClick={this.signUp}>Sign up</button>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cardFlipped: false
+        };
+
+        this.onFlipHandler = this.onFlipHandler.bind(this);
+    }
+
+    onFlipHandler() {
+        this.setState({
+            cardFlipped: !this.state.cardFlipped
+        });
+    }
+
+    signUp() {
+        browserHistory.push('/home');
+    }
+
+    render() {
+        const s = this.state.cardFlipped ? styles.flipped : '';
+
+        return (
+            <div className={styles.content}>
+                <h1 className={styles.heading}>Login Page</h1>
+                <p className={styles.lead}>Create an account to get started!</p>
+                <button className={styles.signUpButton} onClick={this.signUp}>
+                    Sign up
+                </button>
+
+                <section className={styles.cardcontainer}
+                         onClick={this.onFlipHandler}>
+                    <div id="card" className={s}>
+                        <figure className={styles.front}>1</figure>
+                        <figure className={styles.back}>2</figure>
+                    </div>
+                </section>
+
+            </div>
+        );
+    }
 }
