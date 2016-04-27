062311e2394ca347a75d6bb9a383b8c7942f7c36
Vladimir Sizov <sizov.v.f@gmail.com>
2016-04-27 18:38:31 +0100

    changing folder location

--- src/common/components/Routes.js
+++ src/common/components/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './App';
-import LoginPage from '../../pages/login/page';
+import LoginPage from '../../example/page';
 
 
 export default (
--- src/main.js
+++ src/main.js
@@ -1,29 +1,12 @@
-/**
- * App entry point
- */
-
-// Polyfill
 import 'babel-polyfill';
-
-// Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import Example from './example/Example';
+import './base.css';
 
-// Routes
-import Routes from './common/components/Routes';
-
-// Base styling
-import './common/base.css';
-
-
-// ID of the DOM element to mount app on
 const DOM_APP_EL_ID = 'app';
 
 // Render the router
 ReactDOM.render((
-  <Router history={browserHistory}>
-    {Routes}
-  </Router>
+    <Example/>
 ), document.getElementById(DOM_APP_EL_ID));
-
