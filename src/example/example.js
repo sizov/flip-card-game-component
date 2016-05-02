8158d14ca14b46c934cf4afeb07023d6fa94af38
Vladimir Sizov <sizov.v.f@gmail.com>
2016-05-02 09:07:22 +0100

    updating version of game logic

--- src/example/example.js
+++ src/example/example.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import CardsBoard from '../components/cardsBoard/CardsBoard';
 import styles from './style.css';
+import flipCardGame from 'flip-card-game/dist/flip-card-game';
 
-const rndBool = () => !!Math.round(Math.random());
-const getCard = (flipped, id) => ({id, flipped});
+const FlipCardGame = flipCardGame.FlipCardGame;
+const getCard = (flipped, id, image) => ({flipped, id, image});
 
-function getCards() {
-    const result = [];
-    const l = 10 + Math.round(Math.random() * 10);
+const game = new FlipCardGame();
+const gamePlayers = game.getPlayers();
+const gameCards = game.getCards();
 
-    for (let i = 0; i < l; i++) {
-        result.push(getCard(rndBool(), i.toString()));
-    }
+//fixme: import events from game
+const CARD_FLIP_EVENT = 'cardFlipEvent';
+const PLAYER_FOUND_PAIR_EVENT = 'playerFoundPairEvent';
+const PLAYER_FINISHED_FLIPPING_PAIR_EVENT = 'playerFinishedFlippingPairEvent';
+const GAME_OVER_EVENT = 'gameOverEvent';
+const GAME_DRAW_EVENT = 'gameDrawEvent';
 
-    return result;
+game.on(CARD_FLIP_EVENT, ()=>console.log(CARD_FLIP_EVENT));
+game.on(PLAYER_FOUND_PAIR_EVENT, ()=>console.log(PLAYER_FOUND_PAIR_EVENT));
+game.on(PLAYER_FINISHED_FLIPPING_PAIR_EVENT, ()=>console.log(PLAYER_FINISHED_FLIPPING_PAIR_EVENT));
+game.on(GAME_OVER_EVENT, ()=>console.log(GAME_OVER_EVENT));
+game.on(GAME_DRAW_EVENT, ()=>console.log(GAME_DRAW_EVENT));
+
+function getCards(nariveGameGards) {
+    return nariveGameGards.map(function (nativeGameCard) {
+        return getCard(
+            nativeGameCard.getState() !== 'back',
+            nativeGameCard.getId(),
+            nativeGameCard.getPairId()
+        );
+    });
 }
 
 //FIXME: move to utils or use immutable.js
@@ -37,17 +54,22 @@ export default class LoginPage extends React.Component {
         super(props);
 
         this.state = {
-            cards: getCards()
+            players: gamePlayers,
+            currentPlayer: gamePlayers[0],
+            cards: getCards(gameCards)
         };
 
         this.onCardClickHandler = this.onCardClickHandler.bind(this);
+        this.onPlayerChangeHandler = this.onPlayerChangeHandler.bind(this);
     }
 
     onCardClickHandler(cardData) {
-        if (rndBool()) {
-            alert('can\'t flip');
-            return;
-        }
+        console.log(cardData);
+
+        game.flipCard({
+            cardId: cardData.id,
+            playerId: this.state.currentPlayer.getId()
+        });
 
         const cards = immutableUpdate(this.state.cards, {
             item: cardData,
@@ -61,10 +83,36 @@ export default class LoginPage extends React.Component {
         });
     }
 
+    onPlayerChangeHandler(e) {
+        this.setState(Object.assign(
+            this.state,
+            {
+                currentPlayer: e.target.value
+            }
+        ));
+    }
+
     render() {
+        const playersNodes = this.state.players.map(function (player) {
+            const playerId = player.getId();
+            return <option value={playerId}
+                           key={playerId}>
+                {playerId}
+            </option>
+        });
+
         return (
-            <CardsBoard cards={this.state.cards}
-                        onCardClick={this.onCardClickHandler}/>
+            <div>
+                Current player:
+                <select
+                    defaultValue={this.state.currentPlayer.getId()}
+                    onChange={this.onPlayerChangeHandler}>
+                    {playersNodes}
+                </select>
+
+                <CardsBoard cards={this.state.cards}
+                            onCardClick={this.onCardClickHandler}/>
+            </div>
         );
     }
 }
